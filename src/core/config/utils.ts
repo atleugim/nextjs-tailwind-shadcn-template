c064b39e7097d6e4ecca9674083dbbff0e477ece
Miguel Vega <vegaleugim@gmail.com>
2024-02-13 01:28:31 -0500

    added shadcn-ui

--- src/core/config/utils.ts
+++ src/core/config/utils.ts
@@ -1,3 +1,10 @@
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export const cn = (...inputs: ClassValue[]) => {
+  return twMerge(clsx(inputs));
+};
+
 const isValidString = (str: string): boolean => {
   if (
     str === null ||
